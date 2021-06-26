8af38cc2e009b5c029af070148f0746bfbd50e7f
Juan Sebastian Moreno Hernandez <juanse.jm18@gmail.com>
2021-06-25 21:51:09 -0500

    FEAT: Authentication guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./features/features.module').then((m) => m.FeaturesModule),
+  },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
